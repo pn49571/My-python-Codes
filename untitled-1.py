69b449789c016dab31fcc487e43367d35686a8d0
pn49571 <pn49571@umbc.edu>
2018-04-22 02:05:00 +0000

    restructuring folder

--- untitled-1.py
+++
@@ -1,5 +0,0 @@
-def find_emails(s):
-    return re.findall(r'\w*.?\w*@\w+.\w+.?\w*',s)
-
-
-find_emails([kenneth.love@teamtreehouse.com, @support, ryan@teamtreehouse.com, test+case@example.co.uk",'kenneth@teamtreehouse.com', 'ryan@teamtreehouse.com', 'test@example.co.uk'])
