7e475668b07c09f90a05415b2e048aba1da8dee5
Saiadithya (The Sun God) <shaadithya@gmail.com>
2018-06-27 01:06:04 -0400

    Update and rename read-processor-json.py to read-json-type-1.py

--- ForProject/read-json-type-1.py
+++ ForProject/read-json-type-1.py
@@ -1,7 +1,7 @@
 import json
 
-f1 = open('C:\\Users\\SThangaraj\\Downloads\\myoutfile.json','w')
-with open('C:\\Users\\SThangaraj\\Downloads\\flash-2018-04-30T09-07.json') as f:
+f1 = open('XXXXXXXXXXX.json','w')
+with open('XXXXXXXXXXXXXX.json') as f:
     for line in f:
         list_json = (json.loads(line))
         if list_json['timeStamp'][17:19] in ('05', '06', '07', '08', '09', '10', '11', '12', '13', '14'):
